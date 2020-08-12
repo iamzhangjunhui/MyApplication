e56256cfe27ef6e9c3e8dd216e1b06d8c838ae60
kaylee <1126238932@qq.com>
2020-08-12 11:10:13 +0800

    added:(android 7.1新功能：App Shortcuts——实现应用快捷方式 https://www.jianshu.com/p/6b6f79096256?utm_source=desktop&utm_medium=timeline)

---
+++ app/src/main/java/com/loyo/myapplication/shortcuts/TestActivity.kt
@@ -0,0 +1,16 @@
+package com.loyo.myapplication.shortcuts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.loyo.myapplication.R
+
+/**
+ * App Shortcuts——实现应用快捷方式
+ * https://www.jianshu.com/p/6b6f79096256?utm_source=desktop&utm_medium=timeline
+ */
+class TestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
+}
