dd794dfca4c799db23cfca427334db441ae0344b
kaylee <1126238932@qq.com>
2020-10-19 16:48:10 +0800

    added：可以放大缩小图片的view和调用系统邮箱

--- app/src/main/java/com/loyo/myapplication/MainActivity.kt
+++ app/src/main/java/com/loyo/myapplication/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.loyo.myapplication
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ObservableField
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.loyo.myapplication.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 import pub.devrel.easypermissions.AfterPermissionGranted
@@ -20,9 +24,14 @@ class MainActivity : AppCompatActivity() {
             data.set("KAYLEE")
             it.data = data
             it.item = "kaylee"
+            btnEmail.setOnClickListener{
+                val uri = Uri.parse("mailto:1126238932@qq.com")
+                //使用Intent.ACTION_SEND的会调起非邮箱应用
+                val intent = Intent(Intent.ACTION_SENDTO, uri)
+                startActivity(intent)
+            }
         }
     }
-
 
 
 }
